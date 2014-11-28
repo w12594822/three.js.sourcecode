131ba1703028d4c6ff8094d17fb78c83a5aef75f
Hou Chunlei <omni360@qq.com>
2014-11-28 14:32:31 +0800

    Update Scene.js

--- src/scenes/Scene.js
+++ src/scenes/Scene.js
@@ -6,64 +6,87 @@ THREE.Scene = function () {
 
 	THREE.Object3D.call( this );
 
-	this.fog = null;
-	this.overrideMaterial = null;
+	this.fog = null;		//场景的雾效属性
+	this.overrideMaterial = null;		//场景的材质属性,默认为null,如果设置了这个属性,场景中的所有对象渲染成这个材质.
 
-	this.autoUpdate = true; // checked by the renderer
-	this.matrixAutoUpdate = false;
+	this.autoUpdate = true; 	// checked by the renderer
+								//默认为true,表示渲染器每一帧都会检查场景和场景中的对象的矩阵的是否更新,如果为false,场景中的对象将不会被自动更新.
 
-	this.__lights = [];
+	/***********************新版中已经删除********************************
+	this.matrixAutoUpdate = false;		//默认为false,场景中的矩阵是否自动更新,新版中已经删除这个属性.
+	**********************************************************************/
 
-	this.__objectsAdded = [];
-	this.__objectsRemoved = [];
+	this.__lights = [];		//场景中的所有灯光存放在__lights属性数组中,新版中已经删除这个属性
+
+	this.__objectsAdded = [];	//场景中的添加的所有相机和骨骼存放在__objectsAdded属性数组中,新版中已经删除这个属性
+	this.__objectsRemoved = [];	//场景中的删除的所有相机和骨骼存放在__objectsRemoved属性数组中,新版中已经删除这个属性
+	//TODO: 这里的属性啥意思?是新加的?添加的相机应该在children数组内呀?应该是这里为了提高效率将相机和骨骼单独放到这个属性数组中.
 
 };
-
+/*************************************************
+****下面是Scene对象的方法属性定义,继承自Object3D
+**************************************************/
 THREE.Scene.prototype = Object.create( THREE.Object3D.prototype );
 
+/************************************下面的方法已经在新版中删除******************************************
+//
+///__addObject方法用来添加灯光对象到场景的__lights属性数组中,添加相机和骨骼到场景的__objectsAdded
+/// NOTE:新版中已经删除这个方法
+//
+///<summary>__addObject</summary>
+///<param name ="object" type="Light,Carmera,Bone">Light,Carmera,Bone,可以包含子对象</param>
+///<returns type="Scene">返回新的Scene对象</returns>
 THREE.Scene.prototype.__addObject = function ( object ) {
 
-	if ( object instanceof THREE.Light ) {
+	if ( object instanceof THREE.Light ) {		//如果是灯光,
 
-		if ( this.__lights.indexOf( object ) === - 1 ) {
+		if ( this.__lights.indexOf( object ) === - 1 ) {	
 
-			this.__lights.push( object );
+			this.__lights.push( object );	//添加到__lights属性数组中
 
 		}
 
 		if ( object.target && object.target.parent === undefined ) {
 
-			this.add( object.target );
+			this.add( object.target );	
 
 		}
 
-	} else if ( ! ( object instanceof THREE.Camera || object instanceof THREE.Bone ) ) {
+	} else if ( ! ( object instanceof THREE.Camera || object instanceof THREE.Bone ) ) {	//如果是骨骼或者相机
 
-		this.__objectsAdded.push( object );
+		this.__objectsAdded.push( object );	//添加到__objectsAdded属性数组中
 
 		// check if previously removed
+		// 检查对象是否已经被删除
 
-		var i = this.__objectsRemoved.indexOf( object );
+		var i = this.__objectsRemoved.indexOf( object );	//如果对象呗删除过
 
 		if ( i !== - 1 ) {
 
-			this.__objectsRemoved.splice( i, 1 );
+			this.__objectsRemoved.splice( i, 1 );	//从__objectsRemoved数组中删除.
 
 		}
 
 	}
 
-	this.dispatchEvent( { type: 'objectAdded', object: object } );
-	object.dispatchEvent( { type: 'addedToScene', scene: this } );
+	this.dispatchEvent( { type: 'objectAdded', object: object } );	//调度事件
+	object.dispatchEvent( { type: 'addedToScene', scene: this } );	//调度事件
 
-	for ( var c = 0; c < object.children.length; c ++ ) {
+	for ( var c = 0; c < object.children.length; c ++ ) {	//如果对象有子对象
 
-		this.__addObject( object.children[ c ] );
+		this.__addObject( object.children[ c ] );	//将子对象也添加到__objectsAdded属性数组中
 
 	}
 
 };
 
+//
+///__removeObject方法用来从场景的__lights属性数组中删除灯光对象,添加相机和骨骼从场景的__objectsRemoved
+/// NOTE:新版中已经删除这个方法
+//
+///<summary>__removeObject</summary>
+///<param name ="object" type="Light,Carmera,Bone">Light,Carmera,Bone,可以包含子对象</param>
+///<returns type="Scene">返回新的Scene对象</returns>
 THREE.Scene.prototype.__removeObject = function ( object ) {
 
 	if ( object instanceof THREE.Light ) {
@@ -91,6 +114,7 @@ THREE.Scene.prototype.__removeObject = function ( object ) {
 		this.__objectsRemoved.push( object );
 
 		// check if previously added
+		// 检查对象是否已经添加.
 
 		var i = this.__objectsAdded.indexOf( object );
 
@@ -113,6 +137,14 @@ THREE.Scene.prototype.__removeObject = function ( object ) {
 
 };
 
+**************************************************************************/
+
+
+/*clone方法
+///clone方法克隆一个Scene对象,将属性数组分别复制.
+*/
+///<summary>clone</summary>
+///<returns type="Scene">返回克隆的Scene对象</returns>	
 THREE.Scene.prototype.clone = function ( object ) {
 
 	if ( object === undefined ) object = new THREE.Scene();
@@ -123,8 +155,9 @@ THREE.Scene.prototype.clone = function ( object ) {
 	if ( this.overrideMaterial !== null ) object.overrideMaterial = this.overrideMaterial.clone();
 
 	object.autoUpdate = this.autoUpdate;
-	object.matrixAutoUpdate = this.matrixAutoUpdate;
-
-	return object;
+	/***********************新版中已经删除********************************
+	object.matrixAutoUpdate = this.matrixAutoUpdate;	//新版中已经删除
+	**********************************************************************/
+	return object;	//返回克隆的Scene对象
 
 };
