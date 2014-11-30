2fff9f44f97555c0858173a518b2f81f50f03d27
Hou Chunlei <omni360@qq.com>
2014-11-30 17:18:26 +0800

    update

--- src/Three.js
+++ src/Three.js
@@ -105,7 +105,6 @@ THREE.DstColorFactor = 208;
 THREE.OneMinusDstColorFactor = 209;
 THREE.SrcAlphaSaturateFactor = 210;
 
-
 // TEXTURE CONSTANTS
 //纹理常量
 
@@ -114,22 +113,25 @@ THREE.MixOperation = 1;
 THREE.AddOperation = 2;
 
 // Mapping modes
-// 贴图模式
+// 映射模式,有THREE.UVMapping平展映射,THREE.CubeReflectionMapping 立方体反射映射,THREE.CubeRefractionMapping立方体折射映射,THREE.SphericalReflectionMapping球面反射映射,THREE.SphericalRefractionMapping球面折射映射.
+//TODO:添加管道映射
 
-THREE.UVMapping = function () {};
+THREE.UVMapping = function () {};	//平展映射
 
-THREE.CubeReflectionMapping = function () {};
-THREE.CubeRefractionMapping = function () {};
+THREE.CubeReflectionMapping = function () {};	//立方体反射映射
+THREE.CubeRefractionMapping = function () {};	//立方体折射映射
 
-THREE.SphericalReflectionMapping = function () {};
-THREE.SphericalRefractionMapping = function () {};
+THREE.SphericalReflectionMapping = function () {};	//球面反射映射
+THREE.SphericalRefractionMapping = function () {};	//球面折射映射.
 
 // Wrapping modes
-// 覆盖模式
+// 用来指定纹理的覆盖模式,有RepeatWrapping平铺(重复),ClampToEdgeWrapping(夹取),MirroredRepeatWrapping(镜像)
 
-THREE.RepeatWrapping = 1000;
-THREE.ClampToEdgeWrapping = 1001;
-THREE.MirroredRepeatWrapping = 1002;
+THREE.RepeatWrapping = 1000;	//RepeatWrapping平铺(重复),a任何超过1.0的值都被置为0.0。纹理被重复一次。
+								//三维系统中最常用的寻址模式之一。在渲染具有诸如砖墙之类纹理的物体时，如果使用包含一整张砖墙的纹理贴图会占用较多的内存，
+								//通常只需载入一张具有一块或多块砖瓦的较小的纹理贴图，再把它按照重叠纹理寻址模式在物体表面映射多次，就可以达到和使用整张砖墙贴图同样的效果。
+THREE.ClampToEdgeWrapping = 1001;	//ClampToEdgeWrapping(夹取),超过1.0的值被固定为1.0。超过1.0的其它地方的纹理，沿用最后像素的纹理。用于当叠加过滤时，需要从0.0到1.0精确覆盖且没有模糊边界的纹理。
+THREE.MirroredRepeatWrapping = 1002;	//MirroredRepeatWrapping(镜像),每到边界处纹理翻转，意思就是每个1.0 u或者v处纹理被镜像翻转。
 
 // Filters
 // 过滤器
--- src/textures/Texture.js
+++ src/textures/Texture.js
@@ -12,15 +12,45 @@ THREE.Texture = function ( image, mapping, wrapS, wrapT, magFilter, minFilter, f
 	this.name = '';		//纹理名称属性,可有可无
 
 	this.image = image !== undefined ? image : THREE.Texture.DEFAULT_IMAGE;		//纹理的图片,最重要的属性是image，这是一个JavaScript Image类型对象。
-	this.mipmaps = [];
 
-	this.mapping = mapping !== undefined ? mapping : THREE.Texture.DEFAULT_MAPPING;
+	/* 
+		Mipmap
+	    在三维世界中,显示一张图的大小与摄象机的位置有关,近的地方,图片实际象素就大一些,远的地方图片实际象
+	   素就会小一些,就要进行一些压缩,例如一张64*64的图,在近处,显示出来可能是50*50,在远处可能显示出来是20*20.
+       如果只限于简单的支掉某些像素,将会使缩小后的图片损失很多细节,图片变得很粗糙,因此,图形学有很多复杂的方
+       法来处理缩小图片的问题,使得缩小后的图片依然清晰,然而,这些计算都会耗费一定的时间.
 
-	this.wrapS = wrapS !== undefined ? wrapS : THREE.ClampToEdgeWrapping;
-	this.wrapT = wrapT !== undefined ? wrapT : THREE.ClampToEdgeWrapping;
+       Mipmap纹理技术是目前解决纹理分辨率与视点距离关系的最有效途径,它会先将图片压缩成很多逐渐缩小的图片,
+       例如一张64*64的图片,会产生64*64,32*32,16*16,8*8,4*4,2*2,1*1的7张图片,当屏幕上需要绘制像素点为20*20 时，
+       程序只是利用 32*32 和 16*16 这两张图片来计算出即将显示为 20*20 大小的一个图片，这比单独利用 32*32 的
+       那张原始片计算出来的图片效果要好得多，速度也更快.
 
-	this.magFilter = magFilter !== undefined ? magFilter : THREE.LinearFilter;
-	this.minFilter = minFilter !== undefined ? minFilter : THREE.LinearMipMapLinearFilter;
+       参考:http://zh.wikipedia.org/wiki/Mipmap
+       参考:http://staff.cs.psu.ac.th/iew/cs344-481/p1-williams.pdf
+       参考:http://blog.csdn.net/linber214/article/details/3342051
+    */
+
+	this.mipmaps = []; //存放mipmaps的属性数组
+
+
+	this.mapping = mapping !== undefined ? mapping : THREE.Texture.DEFAULT_MAPPING;	//映射模式,有THREE.UVMapping平展映射,THREE.CubeReflectionMapping 立方体反射映射,THREE.CubeRefractionMapping立方体折射映射,THREE.SphericalReflectionMapping球面反射映射,THREE.SphericalRefractionMapping球面折射映射.
+
+	this.wrapS = wrapS !== undefined ? wrapS : THREE.ClampToEdgeWrapping; //横向覆盖模式,默认为THREE.ClampToEdgeWrapping,(夹取),超过1.0的值被固定为1.0。超过1.0的其它地方的纹理，沿用最后像素的纹理。用于当叠加过滤时，需要从0.0到1.0精确覆盖且没有模糊边界的纹理。
+																		  //还有THREE.RepeatWrapping(重复)和THREE.MirroredRepeatWrapping(镜像)
+	this.wrapT = wrapT !== undefined ? wrapT : THREE.ClampToEdgeWrapping; //纵向覆盖模式,默认为THREE.ClampToEdgeWrapping,(夹取),超过1.0的值被固定为1.0。超过1.0的其它地方的纹理，沿用最后像素的纹理。用于当叠加过滤时，需要从0.0到1.0精确覆盖且没有模糊边界的纹理。
+																		  //还有THREE.RepeatWrapping(重复)和THREE.MirroredRepeatWrapping(镜像)
+
+    /*
+    	纹素
+			纹素（英语：Texel，即texture element或texture pixel的合成字）是纹理元素的简称，它是计算机图形纹理空间中的基本单元[1]。如同图像是由像素排列而成，纹理是由纹素排列表示的。
+		纹素可以由图像范围来定义，其范围可以通过一些简单的方法来获取，比如阀值。沃罗诺伊分布可以用来描述纹素之间的空间关系。这就意味着我们可以通过将纹素与其周围的纹素图心的连线的垂直平分线将整个纹理分割成连续的多边形。结果就是每一个纹素图心都会有一个沃罗诺伊多边形将其圈起来。
+		在对三维表面铺设纹理的时候，通过纹理映射技术将纹素映射到恰当的输出图像像素上。在当今的计算机上，这个过程主要是由图形卡完成的。
+		纹理工序起始于空间中的某一位置。这个位置可以是在世界坐标系中，但是一般情况下会设定在物体坐标系中。这样纹理会随着物体运动。然后通过投射的方式将其位置（坐标）从三维矢量值转化为0到1范围的二维矢量值（即uv）。再将这个二维矢量值与纹理的分辨率相乘从而获得纹素的位置。
+		当所需纹素的位置不是整数的时候，需要使用纹理滤镜进行处理。
+	*/
+
+	this.magFilter = magFilter !== undefined ? magFilter : THREE.LinearFilter;	//纹理在放大时的过滤方式,线性过滤
+	this.minFilter = minFilter !== undefined ? minFilter : THREE.LinearMipMapLinearFilter;	//纹理在缩小时的过滤方式,线性过滤
 
 	this.anisotropy = anisotropy !== undefined ? anisotropy : 1;
 
